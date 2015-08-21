e3ec466130661c720d37dd0fda4712481906b620
Enja Reyes <enja.reyes@gmail.com>
2015-08-20 20:28:11 -0500

    styles

--- background.js
+++ background.js
@@ -1,42 +1,26 @@
 
-
-$('<a href=#>Beautify </a>').addClass("sidebarbutton").insertBefore($("#sidebar"));
-
-// $('.sidebarbutton').hover(function(){
-//  $('.sidebarbutton').css("background-color", "#ff8700")},
-//  function(){
-//  $('.sidebarbutton').css("background-color", "#777")
-//  }
-// })
-// 
-
-
-
-$('.sidebarbutton').hover(function() {
-    $( this ).css( "background-color", "#ff8700" );
-  }, function() {
-    $( this ).css( "background-color", "#777" );
-  }
-);
-
-$('.sidebarbutton').css({
-    "padding": "6px 12px 6px 12px", 
-    "display": "inline-block", 
-    "margin": "0px 0px 5px 30px", 
-    "font-size": "16px", 
-    "color": "white", 
-    "font-family": "sans-serif", 
-    "background-color": "#777", 
-    "text-decoration": "none"})
+$('<a href=#>Beautify</a>').addClass("sidebarbutton").insertBefore($("#sidebar"));
 
 $('#sidebar').hide();
-$('#mainbar').css({marginLeft: "12%"});
 
+// $('.sidebarbutton').css({
+//   "padding": "6px 12px 6px 12px",
+//   "display": 'inline-block',
+//   "margin": '0px 0px 5px 30px',
+//   "font-size": '16px',
+//   "color": 'white',
+//   "font-family": 'sans-serif',
+//   "text-decoration": 'none'
+// });
 
-$(".sidebarbutton").on("click", function(e){
+$('.sidebarbutton').on('click', function(e){
     e.preventDefault();
     var codeValues = [];
-    $("code").each(function() { codeValues.push($(this).text()) });
-
-    console.log(codeValues)
+    $("code").each(function() { 
+        codeValues.push($(this).text()) 
+    });
+    // console.log('code',$('code'))
+    // console.log(codeValues)
 })
+
+// $('#mainbar').css({"margin-left":'12%'})
