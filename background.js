f33ac32886e9bbbcc49c51b854effe08c5c448ab
Enja Reyes <enja.reyes@gmail.com>
2015-08-21 10:29:08 -0500

    layout and cleanup

--- background.js
+++ background.js
@@ -1,26 +1,33 @@
 
-$('<a href=#>Beautify</a>').addClass("sidebarbutton").insertBefore($("#sidebar"));
+// $('<a href=#>Beautify</a>').addClass("sidebarbutton").insertBefore($("#sidebar"));
+$("#hmenus").append('<a href=# class="sidebarbutton">Beautify</a>');
 
 $('#sidebar').hide();
 
-// $('.sidebarbutton').css({
-//   "padding": "6px 12px 6px 12px",
-//   "display": 'inline-block',
-//   "margin": '0px 0px 5px 30px',
-//   "font-size": '16px',
-//   "color": 'white',
-//   "font-family": 'sans-serif',
-//   "text-decoration": 'none'
-// });
+$('.sidebarbutton').css({
+    "padding": "5px 12px 5px 12px", 
+    "font-size": "16px", 
+    "color": "white", 
+    "font-family": "sans-serif", 
+    "background-color": "#40E0D0",
+    "text-decoration": "none"
+})
 
-$('.sidebarbutton').on('click', function(e){
-    e.preventDefault();
+$('#sidebar').hide();
+$('#mainbar').css({marginLeft: "12%"});
+
+
+var func = function(){
+    console.log('haaaaaa')
     var codeValues = [];
     $("code").each(function() { 
         codeValues.push($(this).text()) 
     });
-    // console.log('code',$('code'))
-    // console.log(codeValues)
-})
+}
 
-// $('#mainbar').css({"margin-left":'12%'})
+var plusBtn = document.querySelector('.sidebarbutton');
+plusBtn.addEventListener('click', func);
+
+
+
+
